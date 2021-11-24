b32a2f92dfd148d9159426b47f725ced03b258df
CelineT00 <92144584+CelineT00@users.noreply.github.com>
2021-11-24 14:20:14 +0100

    abgabe5

--- Abgabe5/script.js
+++ Abgabe5/script.js
@@ -1,29 +1,93 @@
 var Aufgabe;
 (function (Aufgabe) {
+    var Events = /** @class */ (function () {
+        function Events(person, preis, datum) {
+            this.person = person;
+            this.preis = preis;
+            this.datum = datum;
+        }
+        return Events;
+    }());
     var interpret = document.getElementById("interpret");
     var preis = document.getElementById("preis");
     var datum = document.getElementById("zeit");
     var button = document.getElementById("enter");
-    button.addEventListener("click", function () {
-        var liste = document.createElement("tr");
-        var a = document.createElement("td");
-        var b = document.createElement("td");
-        var c = document.createElement("td");
-        var d = document.createElement("td");
-        var deletebutton = document.createElement("button");
-        deletebutton.innerText = "delete";
-        document.getElementById("table").appendChild(liste);
-        a.innerText = interpret.value;
-        b.innerText = preis.value;
-        c.innerText = datum.value;
-        d.appendChild(deletebutton);
-        liste.appendChild(a);
-        liste.appendChild(b);
-        liste.appendChild(c);
-        liste.appendChild(d);
-        deletebutton.addEventListener("click", deleter);
-        function deleter() {
-            document.getElementById("table").removeChild(liste);
+    var standartevent = new Events("Twice", "44.2", "17.05.2022");
+    var arrayevents = [];
+    arrayevents.push(standartevent);
+    var localstoragearray;
+    var localstoragestring = localStorage.getItem("myArray");
+    localstoragearray = JSON.parse(localstoragestring);
+    update();
+    aktualisierenListe();
+    var stringarray = JSON.stringify(arrayevents);
+    localStorage.setItem("myArray", stringarray);
+    function update() {
+        if (localstoragearray != null && localstoragearray.length > 0) {
+            var _loop_1 = function (index) {
+                var liste = document.createElement("tr");
+                var a = document.createElement("td");
+                var b = document.createElement("td");
+                var c = document.createElement("td");
+                var d = document.createElement("td");
+                var deletebutton = document.createElement("button");
+                deletebutton.innerText = "delete";
+                deletebutton.addEventListener("click", deleter);
+                function deleter() {
+                    document.getElementById("table").removeChild(liste);
+                }
+                document.getElementById("table").appendChild(liste);
+                a.innerText = localstoragearray[index].person;
+                b.innerText = localstoragearray[index].preis;
+                c.innerText = localstoragearray[index].datum;
+                d.appendChild(deletebutton);
+                liste.appendChild(a);
+                liste.appendChild(b);
+                liste.appendChild(c);
+                liste.appendChild(d);
+            };
+            for (var index = 0; index < localstoragearray.length; index++) {
+                _loop_1(index);
+            }
         }
+    }
+    function aktualisierenListe() {
+        if (arrayevents.length > 0 && arrayevents != null) {
+            while (document.getElementById("table").lastChild != document.getElementById("wichtig")) {
+                document.getElementById("table").removeChild(document.getElementById("table").lastChild);
+            }
+            var _loop_2 = function (index) {
+                var zeile = document.createElement("tr");
+                var a1 = document.createElement("td");
+                var b1 = document.createElement("td");
+                var c1 = document.createElement("td");
+                var d1 = document.createElement("td");
+                var deletebutton = document.createElement("button");
+                deletebutton.innerText = "delete";
+                deletebutton.addEventListener("click", deleter);
+                function deleter() {
+                    document.getElementById("table").removeChild(zeile);
+                }
+                document.getElementById("table").appendChild(zeile);
+                a1.innerText = arrayevents[index].person;
+                b1.innerText = arrayevents[index].preis;
+                c1.innerText = arrayevents[index].datum;
+                d1.appendChild(deletebutton);
+                zeile.appendChild(a1);
+                zeile.appendChild(b1);
+                zeile.appendChild(c1);
+                zeile.appendChild(d1);
+            };
+            for (var index = 0; index < arrayevents.length; index++) {
+                _loop_2(index);
+            }
+        }
+    }
+    button.addEventListener("click", function () {
+        var neuEvent = new Events(interpret.value, preis.value, datum.value);
+        arrayevents.push(neuEvent);
+        aktualisierenListe();
+        stringarray = JSON.stringify(arrayevents);
+        localStorage.setItem("myArray", stringarray);
     });
 })(Aufgabe || (Aufgabe = {}));
--- Abgabe5/script.ts
+++ Abgabe5/script.ts
@@ -1,33 +1,108 @@
 namespace Aufgabe {
+
+    class Events {
+        person: string;
+        preis: string;
+        datum: string;
+
+        constructor (person: string, preis: string, datum: string) {
+            this.person = person;
+            this.preis = preis;
+            this.datum = datum;
+        }
+        
+    }
     let interpret: HTMLInputElement = <HTMLInputElement> document.getElementById("interpret") as HTMLInputElement;
     let preis: HTMLInputElement = <HTMLInputElement> document.getElementById("preis") as HTMLInputElement;
     let datum: HTMLInputElement = <HTMLInputElement> document.getElementById("zeit") as HTMLInputElement;
     let button: HTMLElement = document.getElementById("enter");
+    
 
-    button.addEventListener("click", () => { 
-            let liste: HTMLElement = document.createElement("tr");
-            let a: HTMLElement = document.createElement("td");
-            let b: HTMLElement = document.createElement("td");
-            let c: HTMLElement = document.createElement("td");
-            let d: HTMLElement = document.createElement("td");
-            let deletebutton: HTMLElement = document.createElement("button");
-            deletebutton.innerText = "delete";
-            document.getElementById("table").appendChild(liste);
-            a.innerText = interpret.value;
-            b.innerText = preis.value;
-            c.innerText = datum.value;
-            d.appendChild(deletebutton);
-            liste.appendChild(a);
-            liste.appendChild(b);
-            liste.appendChild(c);
-            liste.appendChild(d);
-            deletebutton.addEventListener("click", deleter);
 
-            function deleter (): void {
-            document.getElementById("table").removeChild(liste);
+    let standartevent: Events = new Events("Twice", "44.2", "17.05.2022");
+    let arrayevents: Events[] = [];
+    arrayevents.push(standartevent);
+    let localstoragearray: Events[];
+    let localstoragestring: string = localStorage.getItem("myArray");
+    localstoragearray = JSON.parse(localstoragestring);
+    update();
+    aktualisierenListe();
+    let stringarray: string = JSON.stringify(arrayevents);
+    localStorage.setItem("myArray", stringarray);
+
+    function update(): void {
+        if (localstoragearray != null && localstoragearray.length > 0) {
+            for (let index: number = 0; index < localstoragearray.length; index++) {
+        
+                let liste: HTMLElement = document.createElement("tr");
+                let a: HTMLElement = document.createElement("td");
+                let b: HTMLElement = document.createElement("td");
+                let c: HTMLElement = document.createElement("td");
+                let d: HTMLElement = document.createElement("td");
+                let deletebutton: HTMLElement = document.createElement("button");
+                deletebutton.innerText = "delete";
+                deletebutton.addEventListener("click", deleter);
+
+                function deleter (): void {
+                    document.getElementById("table").removeChild(liste);
+                }
+
+
+                document.getElementById("table").appendChild(liste);
+                a.innerText = localstoragearray[index].person;
+                b.innerText = localstoragearray[index].preis;
+                c.innerText = localstoragearray[index].datum;
+                d.appendChild(deletebutton);
+                liste.appendChild(a);
+                liste.appendChild(b);
+                liste.appendChild(c);
+                liste.appendChild(d);
+
+            }
+        }
+    }
+    function aktualisierenListe(): void {
+        if (arrayevents.length > 0 && arrayevents != null) {
+                            
+            while (document.getElementById("table").lastChild != document.getElementById("wichtig")) {
+                document.getElementById("table").removeChild( document.getElementById("table").lastChild);
+                }
+
+            for (let index: number = 0; index < arrayevents.length; index++) {
+                let zeile: HTMLElement = document.createElement("tr");
+                let a1: HTMLElement = document.createElement("td");
+                let b1: HTMLElement = document.createElement("td");
+                let c1: HTMLElement = document.createElement("td");
+                let d1: HTMLElement = document.createElement("td");
+                let deletebutton: HTMLElement = document.createElement("button");
+                deletebutton.innerText = "delete";
+                deletebutton.addEventListener("click", deleter);
+
+                function deleter (): void {
+                    document.getElementById("table").removeChild(zeile);
+                }
+
+                document.getElementById("table").appendChild(zeile);
+                a1.innerText = arrayevents[index].person;
+                b1.innerText = arrayevents[index].preis;
+                c1.innerText = arrayevents[index].datum;
+                d1.appendChild(deletebutton);
+                zeile.appendChild(a1);
+                zeile.appendChild(b1);
+                zeile.appendChild(c1);
+                zeile.appendChild(d1);
+            }
             
         }
-       
+    }
+    button.addEventListener("click", () => { 
+            let neuEvent: Events = new Events(interpret.value, preis.value, datum.value);
+            arrayevents.push(neuEvent);
+
+            aktualisierenListe();
+            
+            stringarray = JSON.stringify(arrayevents);
+            localStorage.setItem("myArray", stringarray);
     });
     
 }
