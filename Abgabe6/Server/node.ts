f045c59407363f95f9e18af2e7115cb27eeebbaf
CelineT00 <92144584+CelineT00@users.noreply.github.com>
2021-12-07 23:51:45 +0100

    weiter

--- Abgabe6/Server/node.ts
+++ Abgabe6/Server/node.ts
@@ -4,14 +4,24 @@ const hostname: string = "127.0.0.1";
 const port: number = 3000;
 
 const server: http.Server = http.createServer(
-    (request: http.IncomingMessage, response: http.ServerResponse) => {
+  (request: http.IncomingMessage, response: http.ServerResponse) => {
+
       response.statusCode = 200;
       response.setHeader("Content-Type", "text/plain");
-      /*response.setHeader('Access-Control-Allow-Origin', '*'); // bei CORS Fehler */
-      response.end("Hello World");
-    }
-  );
+      response.setHeader("Access-Control-Allow-Origin", "*"); //Erlaubt Zugriff
 
+      //Routing -> Pfade definieren
+      let url: URL = new URL(request.url || "", `http://${request.headers.host}`); //Url element
+
+      switch (url.pathname) { //Pfad wird gesucht
+      
+      case "/":   //Startpunkt des Servers, Mutepfad
+          response.write("Server erreichbar");
+          break;
+      case "/convertDate":
+          break;
+          
+  
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-  });
+  console.log(`Server running at http://${hostname}:${port}`);
+});
